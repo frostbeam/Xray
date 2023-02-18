14234e1fa1f3e89d0c8ce5f8759652d2b98f92aa
llowq <96781765+kenDevXD@users.noreply.github.com>
2023-02-18 13:20:54 +0700

    Update ken.sh

--- ken.sh
+++ ken.sh
@@ -1,27 +1,14 @@
-#!/bin/bash
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
-# My Telegram : https://t.me/aixxy7
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-echo -e "${WB}Update time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds${NC}"
-}
-start=$(date +%s)
-clear
+rm -rf update
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+
 cd /usr/bin
 echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Main Menu${NC}"
 wget -q -O menu "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/menu.sh"
@@ -110,7 +97,6 @@ echo -e "                  ${WB}MENU UPDATED${NC}"
 echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
 echo ""
 rm -f xray
-secs_to_human "$(($(date +%s) - ${start}))"
 echo -e "${YB}[ WARNING ] back to menu!! (Y/N)${NC} "
 read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
