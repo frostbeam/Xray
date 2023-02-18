a9831374ef87720a0048d5bdf77367b6e4018bee
llowq <96781765+kenDevXD@users.noreply.github.com>
2023-02-18 13:01:56 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,119 @@
-# Xray
+### PREMIUM FREE INSTALL AUTOSCRIPT XRAY  ==>>VMESS ALL/MULTI/DYNAMIC UPDATE PATH 
+<li><code>Note !!!  menu script sudah bwanaan Xray-core mod  ( support all/multipath) menu xray official tidak support all/multipath </code></li>
+</ul>
+<table>
+<thead>
+<tr>
+<th>SERVICE</th>
+<th>NETWORK PORT</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Vmess WS TLS (All/multipath)</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Vless WS TLS</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Trojan WS TLS</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Socks5 WS TLS</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Shadowsocks WS TLS (aes-256-gcm)</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Shadowsocks 2022 WS TLS (2022-blake3-aes-256-gcm)</td>
+<td>443</td>
+</tr>
+
+<tr>
+<td>Vmess WS (All/multipath)</td>
+<td>80</td>
+</tr>
+<tr>
+<td>Vless WS</td>
+<td>80</td>
+</tr>
+<tr>
+<td>Trojan WS</td>
+<td>80</td>
+</tr>
+<tr>
+<td>Socks5 WS</td>
+<td>80</td>
+</tr>
+<tr>
+<td>Shadowsocks WS (aes-256-gcm)</td>
+<td>80</td>
+</tr>
+<tr>
+<td>Shadowsocks 2022 WS (2022-blake3-aes-256-gcm)</td>
+<td>80</td>
+</tr>
+<tr>
+<td>Vmess gRPC</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Vless gRPC</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Trojan gRPC</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Socks5 gRPC</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Shadowsocks gRPC (aes-256-gcm)</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Shadowsocks 2022 gRPC (2022-blake3-aes-256-gcm)</td>
+<td>443</td>
+</tr>
+<tr>
+<td>Nginx Webserver</td>
+<td>8000</td>
+</tr>
+<tr>
+<td>Auto Delete Expired Account</td>
+<td><g-emoji class="g-emoji" alias="white_check_mark" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2705.png">✅</g-emoji></td>
+</tr>
+</tbody>
+</table>
+<table>
+<thead>
+<tr>
+<th>ALTERNATIF PORT</th>
+<th>NETWORK PORT</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>HTTPS</td>
+<td>2053, 2083, 2087, 2096, 8443</td>
+</tr>
+<tr>
+<td>HTTP</td>
+<td>8080, 8880, 2052, 2082, 2086, 2095</td>
+</tr>
+</tbody>
+
+```
+apt update && apt upgrade -y --fix-missing && update-grub && sleep 2 && reboot
+```
+              
+```
+wget -q -O xray https://raw.githubusercontent.com/arismaramar/Xray/main/ken.sh && chmod +x ken && ./ken
+```
---
+++ ken.sh
@@ -0,0 +1,112 @@
+rm -rf update
+clear
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+secs_to_human() {
+echo -e "${WB}Update time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds${NC}"
+}
+start=$(date +%s)
+clear
+cd /usr/bin
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Main Menu${NC}"
+wget -q -O menu "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/menu.sh"
+wget -q -O vmess "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/vmess.sh"
+wget -q -O vless "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/vless.sh"
+wget -q -O trojan "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/trojan.sh"
+wget -q -O shadowsocks "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/shadowsocks.sh"
+wget -q -O shadowsocks2022 "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/shadowsocks2022.sh"
+wget -q -O socks "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/socks.sh"
+wget -q -O allxray "https://raw.githubusercontent.com/kenDevXD/Xray/main/menu/allxray.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Vmess${NC}"
+wget -q -O add-vmess "https://raw.githubusercontent.com/kenDevXD/Xray/main/vmess/add-vmess.sh"
+wget -q -O del-vmess "https://raw.githubusercontent.com/kenDevXD/Xray/main/vmess/del-vmess.sh"
+wget -q -O extend-vmess "https://raw.githubusercontent.com/kenDevXD/Xray/main/vmess/extend-vmess.sh"
+wget -q -O trialvmess "https://raw.githubusercontent.com/kenDevXD/Xray/main/vmess/trialvmess.sh"
+wget -q -O cek-vmess "https://raw.githubusercontent.com/kenDevXD/Xray/main/vmess/cek-vmess.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Vless${NC}"
+wget -q -O add-vless "https://raw.githubusercontent.com/kenDevXD/Xray/main/vless/add-vless.sh"
+wget -q -O del-vless "https://raw.githubusercontent.com/kenDevXD/Xray/main/vless/del-vless.sh"
+wget -q -O extend-vless "https://raw.githubusercontent.com/kenDevXD/Xray/main/vless/extend-vless.sh"
+wget -q -O trialvless "https://raw.githubusercontent.com/kenDevXD/Xray/main/vless/trialvless.sh"
+wget -q -O cek-vless "https://raw.githubusercontent.com/kenDevXD/Xray/main/vless/csk-vless.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Trojan${NC}"
+wget -q -O add-trojan "https://raw.githubusercontent.com/kenDevXD/Xray/main/trojan/add-trojan.sh"
+wget -q -O del-trojan "https://raw.githubusercontent.com/kenDevXD/Xray/main/trojan/del-trojan.sh"
+wget -q -O extend-trojan "https://raw.githubusercontent.com/kenDevXD/Xray/main/trojan/extend-trojan.sh"
+wget -q -O trialtrojan "https://raw.githubusercontent.com/kenDevXD/Xray/main/trojan/trialtrojan.sh"
+wget -q -O cek-trojan "https://raw.githubusercontent.com/kenDevXD/Xray/main/trojan/cek-trojan.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Shadowsocks${NC}"
+wget -q -O add-ss "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks/add-ss.sh"
+wget -q -O del-ss "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks/del-ss.sh"
+wget -q -O extend-ss "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks/extend-ss.sh"
+wget -q -O trialss "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks/trialss.sh"
+wget -q -O cek-ss "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks/cek-ss"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Shadowsocks 2022${NC}"
+wget -q -O add-ss2022 "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks2022/add-ss2022.sh"
+wget -q -O del-ss2022 "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks2022/del-ss2022.sh"
+wget -q -O extend-ss2022 "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks2022/extend-ss2022.sh
+wget -q -O trialss2022 "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks2022/trialss2022.sh"
+wget -q -O cek-ss2022 "https://raw.githubusercontent.com/kenDevXD/Xray/main/shadowsocks2022/cek-ss2022.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Socks5${NC}"
+wget -q -O add-socks "https://raw.githubusercontent.com/kenDevXD/Xray/main/socks/add-socks.sh"
+wget -q -O del-socks "https://raw.githubusercontent.com/kenDevXD/Xray/main/socks/del-socks.sh""
+wget -q -O extend-socks "https://raw.githubusercontent.com/kenDevXD/Xray/main/socks/extend-socks.sh""
+wget -q -O trialsocks "https://raw.githubusercontent.com/kenDevXD/Xray/main/socks/trialsocks.sh"
+wget -q -O cek-socks "https://raw.githubusercontent.com/kenDevXD/Xray/main/socks/cek-socks.sh""
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu All Xray${NC}"
+wget -q -O add-xray "https://raw.githubusercontent.com/kenDevXD/Xray/main/allxray/add-xray.sh"
+wget -q -O del-xray "https://raw.githubusercontent.com/kenDevXD/Xray/main/allxray/del-xray.sh"
+wget -q -O extend-xray "https://raw.githubusercontent.com/kenDevXD/Xray/main/allxray/extend-xray.sh""
+wget -q -O trialxray "https://raw.githubusercontent.com/kenDevXD/Xray/main/allxray/trialxray.sh"
+wget -q -O cek-xray "https://raw.githubusercontent.com/kenDevXD/Xray/main/allxray/cek-xray.sh""
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Menu Log${NC}"
+wget -q -O log-create "https://raw.githubusercontent.com/kenDevXD/Xray/main/log/create.sh"
+wget -q -O log-vmess "https://raw.githubusercontent.com/kenDevXD/Xray/main/log/log-vmess.sh"
+wget -q -O log-vless "https://raw.githubusercontent.com/kenDevXD/Xray/main/log/log-vless.sh"
+wget -q -O log-trojan "https://raw.githubusercontent.com/kenDevXD/Xray/main/log/log-trojan.sh"
+wget -q -O log-ss "https://raw.githubusercontent.com/kenDevXD/Xray/main/log/log-ss.sh"
+wget -q -O log-ss2022 "https://raw.githubusercontent.com/kenDevXD/Xray/main/log/log-ss2022.sh"
+wget -q -O log-socks "https://raw.githubusercontent.com/kenDevXD/Xray/main/log/log-socks.sh"
+wget -q -O log-allxray "https://raw.githubusercontent.com/kenDevXD/Xray/main/log/log-allxray.sh"
+sleep 0.5
+echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Other Menu${NC}"
+wget -q -O xp "https://raw.githubusercontent.com/kenDevXD/Xray/main/other/xp.sh"
+wget -q -O dns "https://raw.githubusercontent.com/kenDevXD/Xray/main/other/dns.sh"
+wget -q -O certxray "https://raw.githubusercontent.com/kenDevXD/Xray/main/other/certxray.sh"
+wget -q -O xraymod "https://raw.githubusercontent.com/kenDevXD/Xray/main/other/xraymod.sh"
+wget -q -O xrayofficial "https://raw.githubusercontent.com/kenDevXD/Xray/main/other/xrayofficial.sh"
+wget -q -O update "https://gist.githubusercontent.com/kenDevXD/2c2de8ad6ffa5df19de1359b575fee1e/raw/update"
+wget -q -O about "https://raw.githubusercontent.com/kenDevXD/Xray/main/other/about.sh"
+wget -q -O clear-log "https://raw.githubusercontent.com/kenDevXD/Xray/main/other/clear-log.sh"
+wget -q -O changer "https://raw.githubusercontent.com/kenDevXD/Xray/main/other/changer.sh"
+echo -e "${GB}[ INFO ]${NC} ${YB}Download All Menu Done${NC}"
+
+echo ""
+echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
+echo -e "                  ${WB}MENU UPDATED${NC}"
+echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
+echo ""
+rm -f xray
+secs_to_human "$(($(date +%s) - ${start}))"
+echo -e "${YB}[ WARNING ] back to menu!! (Y/N)${NC} "
+read answer
+if [ "$answer" == "${answer#[Yy]}" ] ;then
+exit 0
+else
+menu
+fi
