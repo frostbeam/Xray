785344df86da34a038b8421757b04e3aa5cae02f
llowq <96781765+kenDevXD@users.noreply.github.com>
2023-03-26 13:15:45 +0700

    Update dns.sh

--- other/dns.sh
+++ other/dns.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-# ANSI Escape Code
 NC='\e[0m'
-## Foreground
 DEFBOLD='\e[39;1m'
 RB='\e[31;1m'
 GB='\e[32;1m'
@@ -10,9 +7,6 @@ BB='\e[34;1m'
 MB='\e[35;1m'
 CB='\e[35;1m'
 WB='\e[37;1m'
-
-# Source ANSI
-# https://ansi.gabebanks.net/
 clear
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
 read -rp "Domain/Host: " -e host
